216dd4a2206a1939fab71ce00f92f982ffc02414
Thống Nguyễn <thong0128@gmail.com>
2024-02-29 14:26:30 +0700

    Done

--- src/main/java/com/codegym/controller/EmployController.java
+++ src/main/java/com/codegym/controller/EmployController.java
@@ -1,2 +1,25 @@
-package com.codegym.controller;public class EmployController {
+package com.codegym.controller;
+
+import com.codegym.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployController {
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employee/info";
+    }
 }
